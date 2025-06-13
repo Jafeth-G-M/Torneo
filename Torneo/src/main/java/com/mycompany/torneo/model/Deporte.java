5f26191103eaa1a9248e6ce5c602369b3c8401d8
Jafeth-Godinez-Matarrita <jafeth.godinez.matarrita@est.una.ac.cr>
2025-06-13 00:31:10 -0600

    Update Deporte.java

--- Torneo/src/main/java/com/mycompany/torneo/model/Deporte.java
+++ Torneo/src/main/java/com/mycompany/torneo/model/Deporte.java
@@ -9,5 +9,34 @@ package com.mycompany.torneo.model;
  * @author jafet
  */
 public class Deporte {
-    
+       private String nombre;
+    private String imagenBalon;
+
+    // Constructor, getters, setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getImagenBalon() {
+        return imagenBalon;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setImagenBalon(String imagenBalon) {
+        this.imagenBalon = imagenBalon;
+    }
+
+    public Deporte() {
+    }
+
+    public Deporte(String nombre, String imagenBalon) {
+        this.nombre = nombre;
+        this.imagenBalon = imagenBalon;
+    }
+
+     
 }
